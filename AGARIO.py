d23c6cd236dd5da32041a28125d5fdeee2e723e4
SamTzz <94377627+SamTzz@users.noreply.github.com>
2021-11-19 17:20:12 +0100

    Add files via upload

---
+++ AGARIO.py
@@ -0,0 +1,95 @@
+import random
+import pygame
+from pygame.math import Vector2
+import core
+from Creep import Creep
+
+
+def setup():
+    print("Setup START---------")
+    core.fps = 60
+    core.WINDOW_SIZE = [1600, 900]
+
+    core.memory("centredecercle", Vector2(100, 200))
+    core.memory("rayonducercle", 12)
+    core.memory("couleurducercle", (255, 0, 0))
+
+    core.memory("direction", Vector2(0, 0))
+    core.memory("Fx", 0)
+    core.memory("Ux", Vector2(0, 0))
+    core.memory("l", 0)
+    core.memory("l0", 0.000001)
+    core.memory("L", 0)
+
+    core.memory("z", Vector2(0, -1))
+    core.memory("q", Vector2(-1, 0))
+    core.memory("s", Vector2(0, 1))
+    core.memory("d", Vector2(1, 0))
+
+    core.memory("F_key", Vector2(0, 0))
+
+    core.memory("Creep", "Creep_n°")
+    core.memory("TableauDeCreeps", [])
+    for i in range(1000):
+        core.memory("TableauDeCreeps").append(Creep())
+    print("Setup END-----------")
+
+
+def run():
+    core.cleanScreen()
+    pygame.draw.circle(core.screen, core.memory("couleurducercle"), core.memory("centredecercle"), core.memory("rayonducercle"))
+
+    if core.getKeyPressList(pygame.K_r):
+        core.memory("direction", Vector2(0, 0))
+
+    if core.getKeyPressList(pygame.K_z):
+        core.memory("F_key", core.memory("F_key") + core.memory("z"))
+        core.memory("direction", Vector2(core.memory("F_key")))
+
+    if core.getKeyPressList(pygame.K_s):
+        core.memory("F_key", core.memory("F_key") + core.memory("s"))
+        core.memory("direction", Vector2(core.memory("F_key")))
+
+    if core.getKeyPressList(pygame.K_q):
+        core.memory("F_key", core.memory("F_key") + core.memory("q"))
+        core.memory("direction", Vector2(core.memory("F_key")))
+
+    if core.getKeyPressList(pygame.K_d):
+        core.memory("F_key", core.memory("F_key") + core.memory("d"))
+        core.memory("direction", Vector2(core.memory("F_key")))
+
+    core.memory("F_key", Vector2(0, 0))
+
+    if core.memory("centredecercle").y < 0:
+        core.memory("direction", Vector2(core.memory("direction").x, core.memory("direction").y+1))
+
+    if core.memory("centredecercle").y > core.WINDOW_SIZE[1]:
+        core.memory("direction", Vector2(core.memory("direction").x, core.memory("direction").y-1))
+
+    if core.memory("centredecercle").x < 0:
+        core.memory("direction", Vector2(core.memory("direction").x+1, core.memory("direction").y))
+
+    if core.memory("centredecercle").x > core.WINDOW_SIZE[0]:
+        core.memory("direction", Vector2(core.memory("direction").x-1, core.memory("direction").y))
+
+    core.memory("centredecercle", core.memory("direction")+core.memory("centredecercle"))
+
+    if core.getMouseLeftClick() is not None:
+        core.memory("Ux", core.getMouseLeftClick() - core.memory("centredecercle"))
+        core.memory("l", core.memory("Ux").length())
+        core.memory("Ux", core.memory("Ux").normalize())
+        core.memory("L", abs(core.memory("l") - core.memory("l0")))
+
+    else:
+        core.memory("Ux", Vector2(0, 0))
+
+    for i in core.memory("TableauDeCreeps"):
+        pygame.draw.circle(core.screen, i.couleur, i.position, i.rayon)
+        print(Creep().position)
+
+    core.memory("Fx", core.memory("Ux"))
+    core.memory("direction", core.memory("Fx"))
+    core.memory("centredecercle", core.memory("direction") + core.memory("centredecercle"))
+
+
+core.main(setup, run)
---
+++ Joueur.py
@@ -0,0 +1,4 @@
+
+
+class Joueur:
+    def __init__(self):
